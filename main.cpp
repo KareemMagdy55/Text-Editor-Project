ff360e77742117ef6963cca3261377c112377f8b
KareemMagdy55 <91559979+KareemMagdy55@users.noreply.github.com>
2022-05-08 18:15:54 +0200

    Merge files function added.

--- main.cpp
+++ main.cpp
@@ -19,11 +19,44 @@
 
 using namespace std;
 
-
+void mergeFiles();
 
 int main() {
-
-
+    mergeFiles();
     return 0;
 }
 
+void mergeFiles(){
+
+    ifstream originalFile ;
+    ifstream newFile ; // the new file to merge with original one.
+    ofstream mergedFile ;
+
+    char name1[81]; // file 1 name
+    char name2[81]; // file 2 name
+
+
+    do {
+        cout << "----------------------------------\n"
+                "Please enter a valid file names.\n"
+                "Please, enter original file name:";
+        cin >> name1;
+        cout << "Please, enter new file name:";
+        cin >> name2;
+
+
+        originalFile.open(name1);
+        newFile.open(name2);
+        mergedFile.open("Merged File.txt");
+
+    }while(!originalFile || !newFile) ; // While file names are not valid, get another file names from user
+
+
+    mergedFile << originalFile.rdbuf() << ' ' << newFile.rdbuf(); // read buffers of file 1 & 2 and save it in mergedFile.
+
+    originalFile.close();
+    newFile.close();
+    mergedFile.close();
+
+
+}
