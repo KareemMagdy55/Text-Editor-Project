9f8c092b21817978e1f313e0641831f2c5040d5a
KareemMagdy55 <91559979+KareemMagdy55@users.noreply.github.com>
2022-05-08 18:15:06 +0200

    Creating the program's initial format and including headers .

--- main.cpp
+++ main.cpp
@@ -1,6 +1,29 @@
+/*
+        Text Editor program
+
+        FCAI – Programming 1 – 2022 - Assignment 3
+
+        Date: 8 May 2022
+
+        Author1 : Abdullah Ibrahim  ID : 20210590
+        Author2 : Kareem Magdy      ID : 20211071
+        Author3 : Mohammed Reyad    ID : 20211074
+
+        Teaching Assistant: Eng. Afaf
+*/
+
+
 #include <iostream>
+#include <fstream>
+#include <regex>
+
+using namespace std;
+
+
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+
     return 0;
 }
+
