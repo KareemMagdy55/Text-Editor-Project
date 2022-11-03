a62dcbb1bb7370f83d17231197b2eddb7fcfe7ac
Kareem Magdy <91559979+KareemMagdy55@users.noreply.github.com>
2022-11-03 23:18:06 +0200

    Delete useless comments

--- main.cpp
+++ main.cpp
@@ -1,18 +1,3 @@
-/*
-
-        Text Editor program
-
-        FCAI – Programming 1 – 2022 - Assignment 4
-
-        Date: 8 May 2022
-
-        Author1 : Abdullah Ibrahim  ID : 20210590
-        Author2 : Kareem Magdy      ID : 20211071
-        Author3 : Mohammed Reyad    ID : 20211074
-
-        Teaching Assistant: Eng. Afaf
-*/
-
 #include <iostream>
 #include <fstream>
 #include <algorithm>
